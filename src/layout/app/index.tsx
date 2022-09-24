d253e98df236d71779b244a1e5fe7513ba9ec483
silver-ts <silvertechsavvy@gmail.com>
2022-09-24 22:31:51 +0200

    Fixed app issues

--- src/layout/app/index.tsx
+++ src/layout/app/index.tsx
@@ -3,19 +3,24 @@ import { PoochBox, PoochContainer } from "../../ui-components";
 import { SizesEnum } from "../../settings/sizes";
 import { useRouter } from "next/router";
 import { ComponentBottombar } from "../../component";
+import styled from "styled-components";
 
 interface Props {
   children: React.ReactNode;
 }
 
+const StyledLayout = styled(PoochBox)`
+  padding-bottom: 120px;
+`;
+
 const App: React.FunctionComponent<Props> = ({ children }) => {
   const router = useRouter();
 
   return (
-    <PoochBox padding={{ y: SizesEnum.Large }} column>
+    <StyledLayout padding={{ y: SizesEnum.Large }} column>
       <PoochContainer fullWidth>{children}</PoochContainer>
       <ComponentBottombar />
-    </PoochBox>
+    </StyledLayout>
   );
 };
 
