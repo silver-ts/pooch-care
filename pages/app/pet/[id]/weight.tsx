251107931c4f3b04d1d4c4079e5afa5c7fc6117b
silver-ts <silvertechsavvy@gmail.com>
2022-09-26 03:14:30 +0200

    Fixed chart labels

--- pages/app/pet/[id]/weight.tsx
+++ pages/app/pet/[id]/weight.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { FormattedMessage, useIntl } from "react-intl";
+import { useIntl } from "react-intl";
 import { LayoutApp } from "../../../../src/layout";
 import { useRouter } from "next/router";
 import usePetsSingle from "../../../../src/api/queries/pets-single";
-import { ComponentCardItem, ComponentCsr, ComponentErrorScreen, ComponentPetCard } from "../../../../src/component";
+import { ComponentCsr, ComponentErrorScreen, ComponentPetCard } from "../../../../src/component";
 import { PoochGrid, PoochLineChart, PoochList } from "../../../../src/ui-components";
 import styled from "styled-components";
 import usePetsWeight from "../../../../src/api/queries/pets-weight";
 import { pipeDate } from "../../../../src/pipe";
-import { createRef } from "react";
+import { useState } from "react";
 
 const StyledPetCard = styled.div`
   @media screen and (min-width: 900px) {
@@ -23,8 +23,14 @@ const App: NextPage = () => {
 
   const { id } = router.query;
 
+  const [containerWidth, setContainerWidth] = useState<number>();
+
   const { pet, petError, refetch: refetchPet } = usePetsSingle(id as string);
   const { petsWeight, petsWeightError, refetch: refetchPetWeight } = usePetsWeight(id as string);
+
+  const handleChangeContainerWidth = ({ width }: { width: number }) => {
+    setContainerWidth(width);
+  };
 
   const handleTryAgain = () => {
     void refetchPet();
@@ -41,13 +47,11 @@ const App: NextPage = () => {
 
       <LayoutApp title={intl.formatMessage({ id: "page.pet_weight.header" })} back>
         {pet && petsWeight && (
-          <PoochGrid mobile={1} desktop={1}>
+          <PoochGrid mobile={1} desktop={1} onSizeChange={handleChangeContainerWidth}>
             <StyledPetCard>
               <ComponentPetCard {...pet} />
             </StyledPetCard>
-            <PoochLineChart
-              data={petsWeight.map(({ raw, formatted, date }) => ({ x: date, y: raw, labelY: formatted }))}
-            />
+            <PoochLineChart width={containerWidth} data={petsWeight.map(({ raw, date }) => ({ x: date, y: raw }))} />
             <PoochList label="KG" items={petsWeight.map((weight) => [weight.formatted, pipeDate(weight.date)]) ?? []} />
           </PoochGrid>
         )}
